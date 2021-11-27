697a0ef590ee422b397c994a22948d4bc7b0b650
Eugene Doudine <eugene.d@trekflow.com>
2021-11-27 10:44:58 +0200

    remove unused code

--- opsit-dsl/src/main/java/org/dudinea/explang/SexpParser.java
+++ opsit-dsl/src/main/java/org/dudinea/explang/SexpParser.java
@@ -13,7 +13,6 @@ import java.util.List;
 import java.util.Locale;
 
 import org.dudinea.explang.atom.*;
-import org.dudinea.explang.ParserException;
 
 @SuppressWarnings({"unchecked","rawtypes","serial"})
 public class SexpParser implements IParser {
