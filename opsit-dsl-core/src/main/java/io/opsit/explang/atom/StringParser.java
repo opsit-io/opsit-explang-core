52f64f4d29188927672a643b6dfebdd201c21d43
Eugene Doudine <eugene.d@trekflow.com>
2022-07-31 02:38:03 +0300

    chore: cleanup
    opsit-dsl-core/src/main/java/io/opsit/explang/atom/StringParser.java:
    reformat, cleanup

--- opsit-dsl-core/src/main/java/io/opsit/explang/atom/StringParser.java
+++ opsit-dsl-core/src/main/java/io/opsit/explang/atom/StringParser.java
@@ -2,18 +2,16 @@ package io.opsit.explang.atom;
 
 import io.opsit.explang.ParseCtx;
 
-
 public  class StringParser implements AtomParser {
-	// FIXME: support escapes
-	@Override
-	public boolean parse(String str, Object[] holder,  ParseCtx pctx) {
-	    if (str.length() >=2 &&
+  @Override
+  public boolean parse(String str, Object[] holder,  ParseCtx pctx) {
+    if (str.length() >=2 &&
 		str.startsWith("\"") &&
 		str.endsWith("\"")) {
-		holder[0]= str.substring(1,str.length()-1);
-		return true;
-	    } else {
-		return false;
-	    }
-	}
+      holder[0]= str.substring(1,str.length()-1);
+      return true;
+    } else {
+      return false;
     }
+  }
+}
