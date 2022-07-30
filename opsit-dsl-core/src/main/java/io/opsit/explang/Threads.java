f9f37c96ad3fa5337ad373df67ba6e2f89dd3008
Eugene Doudine <eugene.d@trekflow.com>
2022-07-31 02:42:14 +0300

    chore: cleanup
    * opsit-dsl-core/src/main/java/io/opsit/explang/Threads.java:
    cleanup, reformat, get rid of warning

--- opsit-dsl-core/src/main/java/io/opsit/explang/Threads.java
+++ opsit-dsl-core/src/main/java/io/opsit/explang/Threads.java
@@ -1,20 +1,17 @@
 package io.opsit.explang;
 
-
-import java.lang.Thread;
-import java.util.Map;
-import java.util.List;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 import java.util.WeakHashMap;
 
 
 public class Threads {
-    public  static final Map <Thread, Compiler.ICtx> contexts =
+  public  static final Map <Thread, Compiler.ICtx> contexts =
 	Collections.synchronizedMap(new WeakHashMap <Thread, Compiler.ICtx>());
-    public  static final Map <Thread, Object> results =
+  public  static final Map <Thread, Object> results =
 	Collections.synchronizedMap(new WeakHashMap<Thread, Object>());
-    public static List sizes() {
+  public static List<Integer> sizes() {
 	return Utils.list(results.size(), contexts.size());
-    }
+  }
 }
