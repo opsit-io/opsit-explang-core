cc46e7e4bdb82bfaea251781aca7adc2116b3156
Eugene Doudine <eugene.d@trekflow.com>
2022-07-31 02:39:27 +0300

    chore: cleanup
    *
    opsit-dsl-core/src/main/java/io/opsit/explang/ParserEOFException.java:
    reformat, cleanup

--- opsit-dsl-core/src/main/java/io/opsit/explang/ParserEOFException.java
+++ opsit-dsl-core/src/main/java/io/opsit/explang/ParserEOFException.java
@@ -1,19 +1,17 @@
 package io.opsit.explang;
 
-import io.opsit.explang.ParseCtx;
-
 public class ParserEOFException
-    extends  ParserException {
+  extends  ParserException {
 
-    public  ParserEOFException (String msg) {
+  public  ParserEOFException (String msg) {
 	super(msg);
-    }
+  }
 
-    public  ParserEOFException (ParseCtx pctx, String msg) {
+  public  ParserEOFException (ParseCtx pctx, String msg) {
 	super(pctx, msg);
-    }
+  }
 
-    public  ParserEOFException (ParseCtx pctx, String msg, Throwable t) {
+  public  ParserEOFException (ParseCtx pctx, String msg, Throwable t) {
 	super(pctx, msg, t);
-    }
+  }
 }
