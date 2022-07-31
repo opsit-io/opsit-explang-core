ad233a5ba15be91bdefe5b7c406650c2ddf136cb
Eugene Doudine <eugene.d@trekflow.com>
2022-07-31 03:43:13 +0300

    chore: reformat, cleanup, get rid of warnings

--- opsit-dsl-core/src/test/java/io/opsit/explang/UtilsTests.java
+++ opsit-dsl-core/src/test/java/io/opsit/explang/UtilsTests.java
@@ -1,6 +1,5 @@
 package io.opsit.explang;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 import org.junit.Test;
@@ -8,38 +7,37 @@ import static io.opsit.explang.Utils.isRoundNumber;
 
 
 public class UtilsTests {
-    @Test
-    public void testIsRoundNumber() {
-        assertFalse(isRoundNumber(null));
-        assertTrue(isRoundNumber(0));
+  @Test
+  public void testIsRoundNumber() {
+    assertFalse(isRoundNumber(null));
+    assertTrue(isRoundNumber(0));
 
-        assertTrue(isRoundNumber(1));
-        assertTrue(isRoundNumber(-1));
+    assertTrue(isRoundNumber(1));
+    assertTrue(isRoundNumber(-1));
 
-        assertTrue(isRoundNumber(Integer.MAX_VALUE));
-        assertTrue(isRoundNumber(Integer.MIN_VALUE));
+    assertTrue(isRoundNumber(Integer.MAX_VALUE));
+    assertTrue(isRoundNumber(Integer.MIN_VALUE));
 
-        assertTrue(isRoundNumber(Long.MAX_VALUE));
-        assertTrue(isRoundNumber(Long.MIN_VALUE));
+    assertTrue(isRoundNumber(Long.MAX_VALUE));
+    assertTrue(isRoundNumber(Long.MIN_VALUE));
         
-        assertTrue(isRoundNumber((double)0.0));
-        assertTrue(isRoundNumber((float)0.0));
+    assertTrue(isRoundNumber((double)0.0));
+    assertTrue(isRoundNumber((float)0.0));
 
-        assertFalse(isRoundNumber((double)-0.1));
-        assertFalse(isRoundNumber((float)-0.1));
+    assertFalse(isRoundNumber((double)-0.1));
+    assertFalse(isRoundNumber((float)-0.1));
 
-        assertFalse(isRoundNumber((double)-10.00001));
-        assertFalse(isRoundNumber((float)-10.00001));
+    assertFalse(isRoundNumber((double)-10.00001));
+    assertFalse(isRoundNumber((float)-10.00001));
 
-        assertTrue(isRoundNumber((double)2e20));
-        assertTrue(isRoundNumber((double)-2e20));
-        assertTrue(isRoundNumber((float)2e20));
-        assertTrue(isRoundNumber((float)-2e20));
+    assertTrue(isRoundNumber((double)2e20));
+    assertTrue(isRoundNumber((double)-2e20));
+    assertTrue(isRoundNumber((float)2e20));
+    assertTrue(isRoundNumber((float)-2e20));
 
-        assertFalse(isRoundNumber((double)2e-20));
-        assertFalse(isRoundNumber((double)-2e-20));
-        assertFalse(isRoundNumber((float)2e-20));
-        assertFalse(isRoundNumber((float)-2e-20));
-
-    }
+    assertFalse(isRoundNumber((double)2e-20));
+    assertFalse(isRoundNumber((double)-2e-20));
+    assertFalse(isRoundNumber((float)2e-20));
+    assertFalse(isRoundNumber((float)-2e-20));
+  }
 }
