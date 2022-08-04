8ff1880293113514edfcc2e49e490a0b4d65126b
Eugene Doudine <dudin@dudineyad.home.trekflow.com>
2022-08-04 22:09:00 +0300

    fix: ClassUtilities fix warnings

--- opsit-dsl-core/src/main/java/io/opsit/explang/ClassUtilities.java
+++ opsit-dsl-core/src/main/java/io/opsit/explang/ClassUtilities.java
@@ -13,8 +13,8 @@ final class ClassUtilities {
    * Mapping from primitive wrapper Classes to their
    * corresponding primitive Classes.
    */
-  private static final Map objectToPrimitiveMap =
-    new HashMap(13);
+  private static final Map<Class<?>, Class<?>> objectToPrimitiveMap =
+    new HashMap<Class<?>, Class<?>>(13);
 
   static {
     objectToPrimitiveMap.put(Boolean.class, Boolean.TYPE);
@@ -32,11 +32,11 @@ final class ClassUtilities {
    * primitive classes whose instances can be assigned an
    * instance of the first.
    */
-  private static final Map primitiveWideningsMap =
-    new HashMap(11);
+  private static final Map<Class<?>, Set<Class<?>>> primitiveWideningsMap =
+    new HashMap<Class<?>, Set<Class<?>>>(11);
 
   static {
-    Set set = new HashSet();
+    Set<Class<?>> set = new HashSet<Class<?>>();
 
     set.add(Short.TYPE);
     set.add(Integer.TYPE);
@@ -45,7 +45,7 @@ final class ClassUtilities {
     set.add(Double.TYPE);
     primitiveWideningsMap.put(Byte.TYPE, set);
 
-    set = new HashSet();
+    set = new HashSet<Class<?>>();
 
     set.add(Integer.TYPE);
     set.add(Long.TYPE);
@@ -54,20 +54,20 @@ final class ClassUtilities {
     primitiveWideningsMap.put(Short.TYPE, set);
     primitiveWideningsMap.put(Character.TYPE, set);
 
-    set = new HashSet();
+    set = new HashSet<Class<?>>();
 
     set.add(Long.TYPE);
     set.add(Float.TYPE);
     set.add(Double.TYPE);
     primitiveWideningsMap.put(Integer.TYPE, set);
 
-    set = new HashSet();
+    set = new HashSet<Class<?>>();
 
     set.add(Float.TYPE);
     set.add(Double.TYPE);
     primitiveWideningsMap.put(Long.TYPE, set);
 
-    set = new HashSet();
+    set = new HashSet<Class<?>>();
 
     set.add(Double.TYPE);
     primitiveWideningsMap.put(Float.TYPE, set);
@@ -89,7 +89,7 @@ final class ClassUtilities {
    * @exception  ClassNotFoundException  if name names an
    * unknown class or primitive
    */
-  static Class classForNameOrPrimitive(String name, ClassLoader loader)
+  static Class<?> classForNameOrPrimitive(String name, ClassLoader loader)
   throws ClassNotFoundException {
     if (name == null || name.equals("") || name.equals("null")
         || name.equals("void"))
@@ -127,7 +127,7 @@ final class ClassUtilities {
    * @return  true if the class is accessible, false otherwise.
    * Presently returns true if the class is declared public.
    */
-  static boolean classIsAccessible(Class aClass) {
+  static boolean classIsAccessible(Class<?> aClass) {
     return Modifier.isPublic(aClass.getModifiers());
   }
 
@@ -145,7 +145,7 @@ final class ClassUtilities {
    * Object or array class in lhs, but not a primitive.
    * @return  true if compatible, false otherwise
    */
-  static boolean compatibleClasses(Class[] lhs, Class[] rhs) {
+  static boolean compatibleClasses(Class<?>[] lhs, Class<?>[] rhs) {
     if (lhs.length != rhs.length)
       return false;
 
@@ -158,8 +158,8 @@ final class ClassUtilities {
       }
 
       if (! lhs[i].isAssignableFrom(rhs[i])) {
-        Class lhsPrimEquiv = primitiveEquivalentOf(lhs[i]);
-        Class rhsPrimEquiv = primitiveEquivalentOf(rhs[i]);
+        Class<?> lhsPrimEquiv = primitiveEquivalentOf(lhs[i]);
+        Class<?> rhsPrimEquiv = primitiveEquivalentOf(rhs[i]);
 
         if (! primitiveIsAssignableFrom(lhsPrimEquiv, rhsPrimEquiv))
           return false;
@@ -181,16 +181,16 @@ final class ClassUtilities {
    * searched, then their superclasses, etc. until a method is
    * found.  Returns null if there is no such method.
    */
-  static Method getAccessibleMethodFrom(Class aClass, String methodName, Class[] parameterTypes) {
+  static Method getAccessibleMethodFrom(Class<?> aClass, String methodName, Class<?>[] parameterTypes) {
     // Look for overridden method in the superclass.
-    Class superclass = aClass.getSuperclass();
+    Class<?> superclass = aClass.getSuperclass();
     Method overriddenMethod = null;
 
     if (superclass != null && classIsAccessible(superclass)) {
       try {
         overriddenMethod =
           superclass.getMethod(methodName, parameterTypes);
-      } catch (NoSuchMethodException _) {
+      } catch (NoSuchMethodException ignored) {
       }
 
       if (overriddenMethod != null)
@@ -201,7 +201,7 @@ final class ClassUtilities {
     // the superclass did not have an override.  Check
     // implemented interfaces.
 
-    Class[] interfaces = aClass.getInterfaces();
+    Class<?>[] interfaces = aClass.getInterfaces();
 
     for (int i = 0; i < interfaces.length; ++i) {
       overriddenMethod = null;
@@ -210,7 +210,7 @@ final class ClassUtilities {
         try {
           overriddenMethod =
             interfaces[i].getMethod(methodName, parameterTypes);
-        } catch (NoSuchMethodException _) {
+        } catch (NoSuchMethodException ignored) {
         }
 
         if (overriddenMethod != null)
@@ -248,10 +248,10 @@ final class ClassUtilities {
    * primitive wrapper.  If aClass is primitive, returns aClass.
    * Otherwise, returns null.
    */
-  static Class primitiveEquivalentOf(Class aClass) {
+  static Class<?> primitiveEquivalentOf(Class<?> aClass) {
     return aClass.isPrimitive()
            ? aClass
-           : (Class) objectToPrimitiveMap.get(aClass);
+           : (Class<?>) objectToPrimitiveMap.get(aClass);
   }
 
   /**
@@ -266,7 +266,7 @@ final class ClassUtilities {
    * does not represent a primitive (e.g. Byte.TYPE), returns
    * false.
    */
-  static boolean primitiveIsAssignableFrom(Class lhs, Class rhs) {
+  static boolean primitiveIsAssignableFrom(Class<?> lhs, Class<?> rhs) {
     if (lhs == null || rhs == null)
       return false;
 
@@ -276,7 +276,7 @@ final class ClassUtilities {
     if (lhs.equals(rhs))
       return true;
 
-    Set wideningSet = (Set) primitiveWideningsMap.get(rhs);
+    Set<?> wideningSet = (Set<?>) primitiveWideningsMap.get(rhs);
 
     if (wideningSet == null)
       return false;
