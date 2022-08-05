bcc08fe31222cd60280d71c39b6fe304a54241b6
Eugene Doudine <dudin@dudineyad.home.trekflow.com>
2022-08-05 10:50:20 +0300

    fix: fix warnings

--- opsit-dsl-core/src/main/java/io/opsit/explang/RangeList.java
+++ opsit-dsl-core/src/main/java/io/opsit/explang/RangeList.java
@@ -11,7 +11,7 @@ import io.opsit.version.Version;;
 //import io.opsit.explang.Funcs.AbstractOp;
 
 
-public class RangeList  implements List {
+public class RangeList  implements List<Number> {
     final private AbstractOp addop = new AbstractOp() {
             @Override
             public  Number doIntOp(Number result, Number arg) {
@@ -57,8 +57,8 @@ public class RangeList  implements List {
     }
 
     @Override
-    public Iterator iterator() {
-        return  new Iterator() {
+    public Iterator<Number> iterator() {
+        return  new Iterator<Number>() {
             final Number from[] = new Number[] {start};
             final boolean inv = step.doubleValue() < 0;
             @Override
@@ -69,7 +69,7 @@ public class RangeList  implements List {
             }
 			    
             @Override
-            public Object next() {
+            public Number next() {
                 final Number result = prom.returnResult(from[0]);
                 final Number r = prom.callOP(addop, from[0], step);
                 from[0] =  prom.returnResult(r);
@@ -105,20 +105,21 @@ public class RangeList  implements List {
         return toArray((Object[])array);
     }
 
+    @SuppressWarnings("unchecked")
     @Override
-    public Object[] toArray(Object[] a) {
+    public Number[] toArray(Object[] a) {
         final int length = size();
         final Object array =
             Array.newInstance(a.getClass().getComponentType(), length);
-        final Iterator iter = this.iterator();
+        final Iterator<Number> iter = this.iterator();
         for (int i = 0; i < length; i++) {
             Array.set(array, i, iter.next());
         }
-        return (Object[])array;
+       return (Number[])array;
     }
 
     @Override
-    public boolean add(Object e) {
+    public boolean add(Number e) {
         throw new UnsupportedOperationException("Cannot use add on RANGE");
     }
 
@@ -128,7 +129,7 @@ public class RangeList  implements List {
     }
 
     @Override
-    public boolean containsAll(Collection c) {
+    public boolean containsAll(Collection<?> c) {
         if (null == c) {
             throw new NullPointerException("containsAll called with null argument");
         }
@@ -141,17 +142,17 @@ public class RangeList  implements List {
     }
 
     @Override
-    public boolean addAll(Collection c) {	
+    public boolean addAll(Collection<? extends Number> c) {	
         throw new UnsupportedOperationException("Cannot use addAll on RANGE");
     }
 
     @Override
-    public boolean removeAll(Collection c) {
+    public boolean removeAll(Collection<?> c) {
         throw new UnsupportedOperationException("Cannot use addAll on RANGE");
     }
 
     @Override
-    public boolean retainAll(Collection c) {
+    public boolean retainAll(Collection<?> c) {
         throw new UnsupportedOperationException("Cannot use addAll on RANGE");			
     }
 
@@ -161,12 +162,12 @@ public class RangeList  implements List {
     }
 
     @Override
-    public boolean addAll(int index, Collection c) {
+    public boolean addAll(int index, Collection<? extends Number> c) {
         throw new UnsupportedOperationException("Cannot use addAll on RANGE");
     }
 
     @Override
-    public Object get(int index) {
+    public Number get(int index) {
         if (index < 0 || index >= size()) {
             throw new IndexOutOfBoundsException();
         }
@@ -179,17 +180,17 @@ public class RangeList  implements List {
     }
 
     @Override
-    public Object set(int index, Object element) {
+    public Number set(int index, Number element) {
         throw new UnsupportedOperationException("Cannot use set on RANGE");
     }
 
     @Override
-    public void add(int index, Object element) {
+    public void add(int index, Number element) {
         throw new UnsupportedOperationException("Cannot use add on RANGE");
     }
 
     @Override
-    public Object remove(int index) {
+    public Number remove(int index) {
         throw new UnsupportedOperationException("Cannot use remove on RANGE");
     }
 
@@ -222,18 +223,19 @@ public class RangeList  implements List {
     }
 		    
     @Override
-    public ListIterator listIterator() {
+    public ListIterator<Number> listIterator() {
+      // FIXME
         throw new NotImplementedException("listIterator");
     }
 
     @Override
-    public ListIterator listIterator(int index) {
+    public ListIterator<Number> listIterator(int index) {
         // FIXME
         throw new NotImplementedException("listIterator");
     }
 
     @Override
-    public List subList(int fromIndex, int toIndex) {
+    public List<Number> subList(int fromIndex, int toIndex) {
         if (fromIndex < 0) {
             throw new IndexOutOfBoundsException("fromIndex="+fromIndex);
         }
