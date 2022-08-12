4dc51aa9fe3920dd5407461dc8a8b6157b4a5a6b
Eugene Doudine <dudinea@opsit.io>
2022-08-12 17:37:51 +0300

    style: fix style warnings

--- src/main/java/io/opsit/explang/ICompiled.java
+++ src/main/java/io/opsit/explang/ICompiled.java
@@ -2,8 +2,10 @@ package io.opsit.explang;
 
 public interface ICompiled {
   public Object evaluate(Backtrace backtrace, Compiler.ICtx ctx);
-  //public ICompiled getInstance();
+
   public void setDebugInfo(ParseCtx pctx);
+
   public void setName(String str);
+
   public ParseCtx getDebugInfo();
 }
