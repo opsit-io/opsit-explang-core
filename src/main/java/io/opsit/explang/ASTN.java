8d786e4c130fe42353c21329b66f3fa1d39b8a4d
Eugene Doudine <dudinea@opsit.io>
2022-08-12 16:54:40 +0300

    style: fix warnings

--- src/main/java/io/opsit/explang/ASTN.java
+++ src/main/java/io/opsit/explang/ASTN.java
@@ -1,37 +1,53 @@
 package io.opsit.explang;
 
-
 // FIXME: must be interface?
+/**
+ * Abstract syntax tree node containing some code object.
+ *
+ * <p>Represents node in abstract syntax tree. A node may be a
+ * leaf node containing a or a list node containing several ASTN nodes.
+ */
 public abstract class ASTN {
+  protected ParseCtx pctx;
+  protected Object object;
+  protected boolean isComment = false;
+  // link to parent ASTN list
+  protected ASTNList parent;
+  // FIXME: need accessor functions
+  public Exception problem;
+
+  /**
+   * Make an ASTN node for a code node.
+   */
   public ASTN(Object object, ParseCtx pctx) {
     this.object = object;
     this.pctx = pctx;
   }
+
+  /**
+   * Make an ASTN node, possible for a comment node.
+   */
   public ASTN(Object object, ParseCtx pctx, boolean isComment) {
     this.object = object;
     this.pctx = pctx;
     this.isComment = isComment;
   }
 
+  /**
+   * Make an ASTN node for an invalid expression.
+   */
   public ASTN(Object object, ParseCtx pctx, Exception ex) {
     this.object = object;
     this.pctx = pctx;
     this.problem = ex;
   }
-    
-  protected ParseCtx pctx;
-  protected Object object;
-  protected boolean isComment = false;
-  protected ASTNList parent;
-  // FIXME: need accessor functions
-  public  Exception problem;
 
   public abstract boolean hasProblems();
 
   public Exception getProblem() {
     return problem;
   }
-    
+
   public ParseCtx getPctx() {
     return pctx;
   }
@@ -43,44 +59,39 @@ public abstract class ASTN {
   }
 
   public abstract Object getObject();
-  //  if (!isList()) {
-  //      return object;
-  //  }
-  //  throw new CompilationException(pctx,"internal error: object requested on list AST Node");
-  // }
-    
-  //public abstract List<ASTN> getList() ;
-  //  if (isList()) {
-  //      return (List<ASTN>) object;
-  //  }
-  //  throw new CompilationException(pctx,"internal error: list requested on non-list AST Node");
-  // }
 
   public ASTN getParent() {
     return this.parent;
   }
 
+  @Override
   public String toString() {
-    return
-      (null != problem  ? "<"+problem+">" : "") +
-      ((null != object) ? object.toString() : "<null>");
+    return (null != problem ? "<" + problem + ">" : "")
+        + ((null != object) ? object.toString() : "<null>");
   }
 
+  /**
+   * Print out a node with source code location and error messages.
+   */
   public String toStringWithPos() {
-    return
-      "@"+this.getPctx()+"@"+
-      (null != problem  ? "<"+problem+">" : "") +
-      ((null != object) ? object.toString() : "<null>");
+    return "@"
+        + this.getPctx()
+        + "@"
+        + (null != problem ? "<" + problem + ">" : "")
+        + ((null != object) ? object.toString() : "<null>");
   }
 
-  public abstract interface  Walker {
+  public abstract interface Walker {
     public void walk(ASTN node);
   }
 
+  /**
+   * dispatch walker run on ASTN tree.
+   */
   public void dispatchWalker(Walker walker) {
     walker.walk(this);
     if (this.isList()) {
-      for(ASTN node : ((ASTNList)this)) {
+      for (ASTN node : ((ASTNList) this)) {
         if (node != null) {
           node.dispatchWalker(walker);
         }
