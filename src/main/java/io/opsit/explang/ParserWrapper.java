78c27a591b83577dbd8440dde8571bbba37497f9
Eugene Doudine <dudinea@opsit.io>
2022-08-13 10:10:01 +0300

    style: fix style warnings

--- src/main/java/io/opsit/explang/ParserWrapper.java
+++ src/main/java/io/opsit/explang/ParserWrapper.java
@@ -1,44 +1,44 @@
 package io.opsit.explang;
 
-
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 
-import java.io.FileInputStream;
-
-
-public class ParserWrapper  {
+public class ParserWrapper {
   IParser parser;
   public static String DEFAULT_INPUT_NAME = "<INPUT>";
-    
+
   public ParserWrapper(IParser parser) {
     this.parser = parser;
   }
-    
+
   public ASTNList parse(String txt) throws ParserException {
     return parse(txt, DEFAULT_INPUT_NAME);
   }
 
   public ASTNList parse(String txt, String inputName) throws ParserException {
     ParseCtx pctx = new ParseCtx(inputName);
-    return (ASTNList)parser.parse(pctx, txt);
+    return (ASTNList) parser.parse(pctx, txt);
   }
 
   public ASTNList parse(InputStream is) {
-    return (ASTNList)parse(is, DEFAULT_INPUT_NAME);
+    return (ASTNList) parse(is, DEFAULT_INPUT_NAME);
   }
 
+  /**
+   * Parse input file with given name as name of input.
+   */
   public ASTNList parse(File file, String inputName) throws ParserException {
     ParseCtx pctx = new ParseCtx(inputName);
-    InputStream is =  null;
+    InputStream is = null;
     try {
       is = new FileInputStream(file);
       InputStreamReader reader = new InputStreamReader(is);
-      return (ASTNList)parser.parse(pctx, reader, Integer.MAX_VALUE);
+      return (ASTNList) parser.parse(pctx, reader, Integer.MAX_VALUE);
     } catch (FileNotFoundException ex) {
       throw new ParserException(pctx, "I/O exception", ex);
     } finally {
@@ -51,15 +51,13 @@ public class ParserWrapper  {
       }
     }
   }
-    
+
   public ASTNList parse(File file) throws ParserException {
     return parse(file, file.getName());
   }
-    
+
   public ASTNList parse(InputStream is, String inputName) {
     Reader reader = new InputStreamReader(is);
-    return (ASTNList)parser.parse(new ParseCtx(inputName), reader,
-                                  Integer.MAX_VALUE); 
+    return (ASTNList) parser.parse(new ParseCtx(inputName), reader, Integer.MAX_VALUE);
   }
-
 }
